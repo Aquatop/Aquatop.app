775fd158a08a094e35063e6f7ec3140af83459a6
Lucas Siqueira <lucassiqzro@gmail.com>
2019-10-14 17:30:28 -0300

    is53 - init react-native app
    Signed-off-by: Lucas Siqueira <lucassiqzro@gmail.com>
    Co-authored-by: LucasLermen <lucas.arthur.lermen@gmail.com>
    Co-authored-by: lukassxp <lpalucas.10@gmail.com>

---
+++ aquatop/src/Index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import Teste from '~/Teste';
+
+// import { Container } from './styles';
+
+export default function App() {
+  return <Teste />;
+}
